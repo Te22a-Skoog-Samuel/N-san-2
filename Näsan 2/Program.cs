019443b0c6013f99f904837dd895baa40c04f2cf
Samuel Skoog <samule.skoog@elev.ga.ntig.se>
2024-11-22 13:27:12 +0100

    några steg gjorda

---
+++ Näsan 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// string [] stringArray = {"bil", "tåg", "boll", "fidgetspinner", "batman"};
+
+// for (int i = 0; i < stringArray.Length; i++)
+// {
+//     Console.WriteLine(stringArray[i]);
+// }
+// Console.ReadLine();
+
+string [] stringArray = {"Alex", "Andreas", "Vincent", "Mande", "Noah"};
+
+int [] numbers = { 1, 3, 5, 7, 10};
+
+
